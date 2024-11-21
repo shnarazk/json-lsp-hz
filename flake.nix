299a69ea533e51de67b550a7b68bf97995432214
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-11-21 18:34:57 +0900

    Add more

---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  description = "WIP: JSON language server in Rust";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  outputs =
+    { self, nixpkgs }:
+    {
+      packages = builtins.listToAttrs (
+        map
+          (
+            system: with import nixpkgs { system = "${system}"; }; {
+              name = system;
+              value = {
+                default = rustPlatform.buildRustPackage rec {
+                  version = "0.1.0-20241121";
+                  name = "json-lsp-hz-${version}";
+                  pname = "json-lsp-hz";
+                  src = self;
+                  cargoHash = "sha256-pAe+UJjNMyCGn2vkZo2+jxLsuzmASMoFAUB/FE7oJjM=";
+                  buildInputs = rustc.buildInputs ++ lib.optional stdenv.isDarwin [ libiconv ];
+                  buildPhase = "cargo build --release";
+                  installPhase = ''
+                    mkdir -p $out/bin;
+                    install -t $out/bin target/release/json-lsp-hz
+                  '';
+                };
+              };
+            }
+          )
+          [
+            "x86_64-linux"
+            "aarch64-linux"
+            "x86_64-darwin"
+            "aarch64-darwin"
+          ]
+      );
+      devShells = builtins.listToAttrs (
+        map
+          (
+            system:
+            with import nixpkgs {
+              system = "${system}";
+              pkgs = nixpkgs.legacyPackages.${system};
+            }; {
+              name = system;
+              value = {
+                default = pkgs.mkShell {
+                  packages = [
+                    pkgs.bashInteractive
+                    pkgs.cargo-watch
+                    pkgs.lldb_20
+                    pkgs.libiconv
+                    pkgs.samply
+                    pkgs.tokei
+                  ];
+                };
+              };
+            }
+          )
+          [
+            "x86_64-linux"
+            "aarch64-linux"
+            "x86_64-darwin"
+            "aarch64-darwin"
+          ]
+      );
+    };
+}
